9786c7a030aa3c20bb9d0aa238055d2c22303baa
Wayne1997 <gis1on1995@outlook.com>
2021-03-01 20:09:01 +0800

    js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+setInterval(function() {
+    var now = new Date();
+    var bday = new Date(1995, 07, 20);
+    var diff = now - bday;
+    document.getElementById("age").innerText = (diff/365/3600/24/1000).toString().substring(0, 14).padEnd(14, '0');
+}, 50)
+
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-140320135-2');
