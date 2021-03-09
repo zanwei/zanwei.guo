8535d52ef285f0a07a5faad2fc12d18ec48ec64b
zanwei <40333372+zanwei@users.noreply.github.com>
2021-03-09 19:13:18 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 setInterval(function() {
     var now = new Date();
     var bday = new Date(1995, 07, 20);
@@ -11,3 +12,43 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'UA-140320135-2');
+
+// img-------------------------------------
+
+window.onload = () => {
+    var elements = document.getElementsByTagName("a")
+    for (var i = 0; i < elements.length; i++){
+        elements[i].addEventListener("mouseover", mouseOver);
+        elements[i].addEventListener("mouseout", mouseOut);
+    }
+}
+
+function mouseOver(e) {
+	switch(e.srcElement.id){
+    	case "location-1": {
+        	document.getElementById("taiyuan").style.display = "block";
+            break;
+        }
+        case "location-2": {
+        	document.getElementById("shanghai").style.display = "block";
+            break;
+        }
+    }
+}
+
+function mouseOut(e) {
+	switch(e.srcElement.id){
+    	case "location-1": {
+			document.getElementById("taiyuan").style.display = "none";
+            break;
+        }
+        case "location-2": {
+        	document.getElementById("shanghai").style.display = "none";
+            break;
+        }
+    }
+}
+
+
+
+
