77a0672a743c2b86acdbf0f11c344c11a085e842
Wayne1997 <gis1on1995@outlook.com>
2021-08-19 21:46:52 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
 
 // Age-------------------------------------
 
+function isLeapYear(year) {
+  return year % 100 == 0 ? year % 400 == 0 : year % 4 == 0;
+}
+
 setInterval(function() {
-    var now = new Date();
-    var bday = new Date(1995, 07, 20);
-    var diff = now - bday;
-    document.getElementById("age").innerText = (diff/366/3600/24/1000).toString().substring(0, 14).padEnd(14, '0');
+  var now = new Date();
+  var bday = new Date(1995, 07, 20);
+
+  var lastBday = null
+
+  if (now.getMonth() >= bday.getMonth() && now.getDate() >= bday.getDate()) {
+    lastBday = new Date(now.getFullYear(), bday.getMonth(), bday.getDate());
+  } else {
+    lastBday = new Date(now.getFullYear() - 1, bday.getMonth(), bday.getDate());
+  }
+
+  var fullYears = lastBday.getFullYear() - bday.getFullYear();
+  var diff = now - lastBday;
+
+  var maxLen = 14;
+
+  var ageStr = (fullYears + diff/365/3600/24/1000).toString().substring(0, maxLen);
+
+  if (!ageStr.includes(".")) {
+    ageStr += ".";
+  }
+  
+  document.getElementById("age").innerText = ageStr.padEnd(maxLen, '0');
 }, 50)
+
 
 // Google Track-------------------------------------
 
